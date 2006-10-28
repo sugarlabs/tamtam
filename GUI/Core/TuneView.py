5453677e0ca006abaf688fe20caf7557d5dd597b
james <james@loggerhead.localdomain>
2006-10-28 02:15:18 +0200

    uses own pagelist

--- GUI/Core/TuneView.py
+++ GUI/Core/TuneView.py
@@ -17,20 +17,17 @@ class TuneView( gtk.ScrolledWindow ):
     def _page_width(self):
         return self.pageContainer.get_allocation().width / GUIConstants.NUMBER_OF_PAGE_BANK_COLUMNS
 
-    def __init__( self, selectPageCallback, tunePagesCallback ):
+    def __init__( self, selectPageCallback ):
         gtk.ScrolledWindow.__init__( self )
         
         #selectPageCallback(): currently connected to pagePlayer.setPlayTune, which skips to a given page of the tune.
         self.selectPageCallback = selectPageCallback
         self.selectedPageIndex = self.NO_PAGE
 
-        #tunePagesCallback(): returns pagePlayer's list of pageIDs
-        self.tunePagesCallback = tunePagesCallback
-
         self.set_policy( gtk.POLICY_ALWAYS, gtk.POLICY_AUTOMATIC )
         self.set_placement( gtk.CORNER_TOP_LEFT )
 
-        #self.pageViews: list of our custom PageView widgets, which should always correspond to tunePagesCallback
+        #self.pageViews: list of our custom PageView widgets
         self.pageViews = [] 
         self.pageContainer = gtk.HBox( False )
         self.add_with_viewport( self.pageContainer )
@@ -49,12 +46,13 @@ class TuneView( gtk.ScrolledWindow ):
         pageID = int( selectionData.data)
 
         if info == 10:
-            self.addPage( pageID, min( x / self._page_width(), len( self.tunePagesCallback() )), True )
+            self.addPage( pageID, min( x / self._page_width(), len( self.pageViews )), True )
         elif info == 11:
-            self.moveSelectedPage( min( x / self._page_width(), len( self.tunePagesCallback() ) -1), True)
+            self.moveSelectedPage( min( x / self._page_width(), len( self.pageViews ) -1), True)
 
     #public method: called by MainWindow on file load
     def syncFromPagePlayer(self):
+        raise 'never call this'
         map( lambda pv:pv.destroy(), self.pageViews )
         self.pageViews = []
         tunePages = self.tunePagesCallback()
@@ -62,10 +60,7 @@ class TuneView( gtk.ScrolledWindow ):
             self.addPage( tunePages[i], i, False)
 
 
-    def addPage( self, pageID, position, mess_with_tunePages = True ):
-        #NOTE: sneaky to manipulate pagePlayer's data struct this way.
-        if mess_with_tunePages : self.tunePagesCallback().insert( position, pageID )
-        
+    def addPage( self, pageID, position ):
         #create a new widget
         pageView = TunePageView( pageID, position, self.selectPage )
         self.pageViews.insert( position, pageView )
@@ -88,8 +83,7 @@ class TuneView( gtk.ScrolledWindow ):
             gtk.gdk.ACTION_COPY|gtk.gdk.ACTION_MOVE )
         
 
-    def moveSelectedPage( self, position, mess_with_tunePages = True ):
-        if mess_with_tunePages : swap(self.tunePagesCallback(), self.selectedPageIndex, position)
+    def moveSelectedPage( self, position):
 
         self.pageContainer.reorder_child( self.pageViews[self.selectedPageIndex], position )
 
@@ -108,8 +102,6 @@ class TuneView( gtk.ScrolledWindow ):
 
         if not self.pageViews[ selectedPageIndex ].selected:
             map( lambda pv: pv.setSelected( pv.pageIndex == selectedPageIndex), self.pageViews)
-
-            #print 'TuneView::selectPage ', self.tunePagesCallback()
 
             if invokeCallback:
                 self.selectPageCallback( selectedPageIndex )
