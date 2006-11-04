3ad0fc69bc33c211a4fc6b3449aac15e60a15516
james <james@loggerhead.localdomain>
2006-11-04 01:48:30 +0100

    extra bonus whitespace

--- TamTam.py
+++ TamTam.py
@@ -35,6 +35,7 @@ if __name__ == "__main__":
         gtk.main()
         CSoundClient.initialize(False)
         sys.exit(0)
+
     def run_edit_mode():
         CSoundClient.initialize(True)
         CSoundClient.setMasterVolume(100)
