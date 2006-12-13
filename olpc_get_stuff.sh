f6d6b9ba8f4aa2f4ea2dba5995474be821e9eef3
james <james@loggerhead.localdomain>
2006-12-13 21:51:55 +0100

    new univorc

--- olpc_get_stuff.sh
+++ olpc_get_stuff.sh
@@ -1,7 +1,10 @@
 
-yum install vim-common vim-enhanced screen git-core
+yum install vim-common vim-enhanced screen git-core fluxbox ctags
 
 echo 'please ensure your ssh key is in place, then type your git-repo username: '
+echo 'copy over .vim* from somewhere'
+echo 'export GIT_AUTHOR_NAME GIT_COMMITTER_NAME in .bashrc'
+echo 'edit olpc's .xinitrc file to change the window-manager'
 
 read USER
 
@@ -10,4 +13,3 @@ cd cvs
 git-clone "git+ssh://$USER@dev.laptop.org/git/projects/tamtam" tamtam
 
 
-echo 'edit olpc's .xinitrc file to change the window-manager'
