9681773634dd158c99864d3ff1bc9f4b07120556
James <olpc@localhost.localdomain>
2007-01-20 19:55:31 -0500

    m

--- Util/Clooper/cmd_csound.c
+++ Util/Clooper/cmd_csound.c
@@ -1,3 +1,19 @@
 
 #include <stdio.h>
 #include "csound.hpp"
+
+int main( int argc, char ** argv)
+{
+    CSOUND * csound = csoundCreate(0);
+    csoundInitialize(&argc, &argv, 0);
+
+    int rval = csoundCompile(csound, argc, argv);
+
+    if (!rval)
+    {
+        while (csoundPerformKsmps(csound) == 0) 
+            ;
+    }
+    csoundDestroy(csound);
+    return rval;
+}
