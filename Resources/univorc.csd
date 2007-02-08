67694bf826d1b244cd1e0753b51210a7687d530d
Oli <olpc@localhost.localdomain>
2007-02-08 16:52:37 -0500

    SynthLab fine tunning

--- Resources/univorc.csd
+++ Resources/univorc.csd
@@ -270,7 +270,8 @@ elseif iSourceType == 5 then
     elseif int(iPar1) == 2 then
         ar gauss   5000*kpara4
     endif
-    aSource butterbp ar, kpara2, kpara3
+    knoisebandwith limit abs(kpara3), 1, sr/2
+    aSource butterbp ar, kpara2, knoisebandwith
     aSource balance aSource, ar
 elseif iSourceType == 6 then
     iSndpitch = p4/261.626
@@ -284,8 +285,8 @@ elseif iSourceType == 6 then
     if icd == 0 then
         icd = .01
     endif
-    aSource	     flooper2	kpara4*.4, iSndpitch*kpara1, ils, ile, icd, 5000+iPar2
-    aSource butterlp aSource, kpara3
+    aSource	     flooper2	kpara4*.4, iSndpitch*abs(kpara1), ils, ile, icd, 5000+iPar2
+    aSource butterlp aSource, abs(kpara3)
 elseif iSourceType == 7 then
     kvoy    =  int(kpara2*3)
     kform1  table   kvoy, 4
@@ -316,7 +317,7 @@ elseif iSourceType == 8 then
     itabdur = nsamp(itable)
     ifreq = 1 / igrdur
     kamp = kpara4 * .2
-    aindex upsamp kpara3 * itabdur 
+    aindex upsamp abs(kpara3) * itabdur 
     atrans upsamp kpara1 * igrdur * iSndPitch
 
     as1 synthGrain aindex, atrans, ifreq, 0.82, irealTable, itabdur
@@ -361,7 +362,8 @@ iPar3   table   ioffset2+2, 5202
 iPar4   table   ioffset2+3, 5202
 
 if iFxType == 1 then
-    aFx	wguide1	as, abs(kpara1)+1, kpara2, kpara3
+    kwgfeed limit kpara3, 0, 1
+    aFx	wguide1	as, abs(kpara1)+1, kpara2, kwgfeed
     aFx	=		aFx*kpara4
 elseif iFxType == 2 then
     aFx	lpf18	as*.0005, abs(kpara1)+20, kpara2, kpara3
@@ -370,11 +372,12 @@ elseif iFxType == 3 then
     aFx bqrez   as*kpara4, abs(kpara1)+20, abs(kpara2)+1, int(iPar3)
     aFx balance aFx, as*kpara4
 elseif iFxType == 4 then
-    amod lfo kpara2, kpara1, int(iPar3)
-    aFx = (as*amod)*kpara4
+    amod lfo 1, kpara1, int(iPar3)
+    aFx = ((as*amod*kpara2)+(as*(1-kpara2)))*kpara4
 elseif iFxType == 5 then
     ain =   as*kpara4
-    arev reverb ain, kpara1
+    krevLength limit kpara1, 0.01, 10
+    arev reverb ain, krevLength
     arev butterlp arev, kpara2
     aFx =   (arev*kpara3)+(as*(1-kpara3))
 elseif iFxType == 6 then
--- SynthLab/SynthLabConstants.py
+++ SynthLab/SynthLabConstants.py
@@ -38,24 +38,24 @@ class SynthLabConstants:
     FLOAT = [.01, False]
     INTEGER = [1, 1]
 
-    # s1 s2 s3 s4 s1min s1max s2min s2max s3min s3max [s1step s1digits] [s2step s2digits] [s3step s3digits]
-    TYPES = {   'lfo': [.5, 1, 0, 0, 0, 1, 0, 20, 0, 5, FLOAT, FLOAT, INTEGER],
-                'rand': [.5, 1.5, 2, 0, 0, 2, 0, 2, 0, 20, FLOAT, FLOAT, FLOAT],
-                'adsr': [.02, .05, .8, .1, 0, 1, 0, 1, 0, 1, FLOAT, FLOAT, FLOAT],
-                'fm': [1, .5, 5, 1, 0, 2, 0, 2, 0, 10, FLOAT, FLOAT, FLOAT],
-                'buzz': [1, 30, .85, 1, 0, 2, 0, 40, 0, 1, FLOAT, INTEGER, FLOAT],
-                'vco': [1, 1, .2, 1, 0, 2, 0, 2, 0, .5, FLOAT, INTEGER, FLOAT],
-                'pluck': [1, 5000, 0, 1, 0, 2, 100, 8000, 0, 8, FLOAT, INTEGER, FLOAT],
-                'noise': [0, 3000, 4000, 1, 0, 2, 0, 8000, 0, 6000, INTEGER, FLOAT, FLOAT],
-                'sample': [1, 5, 5000, 1, 0, 2, 0, 99, 100, 8000, FLOAT, INTEGER, FLOAT],
-                'voice': [1, 3, 5, 1, 0, 2, 0, 8, 1, 10, FLOAT, INTEGER, FLOAT],
-                'grain': [1, 4, 1, 1, 0, 2, 0, 99, 0, 1, FLOAT, INTEGER, FLOAT],
-                'wguide': [100, 3000, .8, 1, 0, 200, 100, 5000, 0, 1, FLOAT, FLOAT, FLOAT],
-                'distort': [800, .7, .7, 1, 0, 1000, 0, 1, 0, 1, FLOAT, FLOAT, FLOAT],
-                'filter': [1000, .6, 0, 1, 200, 5000, 0, 1, 0, 2, FLOAT, FLOAT, INTEGER],
-                'ring': [500, 1, 0, 1, 0, 1000, 0, 1, 0, 5, FLOAT, FLOAT, INTEGER],
-                'reverb': [1.5, 3000, .5, 1, 0, 4, 100, 7000, 0, 1, FLOAT, FLOAT, FLOAT],
-                'harmon': [1.25, .04, .5, 1, 0, 2, 0, 1, 0, 1, FLOAT, FLOAT, FLOAT]}
+    # s1 s2 s3 s4 s1min s1max s2min s2max s3min s3max s4min s4max [s1step s1digits] [s2step s2digits] [s3step s3digits]
+    TYPES = {   'lfo': [.5, 1, 0, 0, 0, 1, 0, 20, 0, 5, 0, 1,  FLOAT, FLOAT, INTEGER],
+                'rand': [.5, 1.5, 2, 0, 0, 2, 0, 2, 0, 20, 0, 1, FLOAT, FLOAT, FLOAT],
+                'adsr': [.02, .05, .8, .1, 0, 1, 0, 1, 0, 1, 0, 1, FLOAT, FLOAT, FLOAT],
+                'fm': [1, .5, 5, 1, 0, 2, 0, 2, 0, 10, 0, 2, FLOAT, FLOAT, FLOAT],
+                'buzz': [1, 30, .85, 1, 0, 2, 0, 40, 0, 1, 0, 2, FLOAT, INTEGER, FLOAT],
+                'vco': [1, 1, .2, 1, 0, 2, 0, 2, 0, .5, 0, 2, FLOAT, INTEGER, FLOAT],
+                'pluck': [1, 5000, 0, 1, 0, 2, 100, 8000, 0, 8, 0, 2, FLOAT, INTEGER, FLOAT],
+                'noise': [0, 3000, 4000, 1, 0, 2, 0, 8000, 0, 6000, 0, 2, INTEGER, FLOAT, FLOAT],
+                'sample': [1, 5, 5000, 1, 0, 2, 0, 99, 100, 8000, 0, 2, FLOAT, INTEGER, FLOAT],
+                'voice': [1, 3, 5, 1, 0, 2, 0, 8, 1, 10, 0, 2, FLOAT, INTEGER, FLOAT],
+                'grain': [1, 4, 1, 1, 0, 2, 0, 99, 0, 1, 0, 2, FLOAT, INTEGER, FLOAT],
+                'wguide': [100, 3000, .8, 1, 0, 200, 100, 5000, 0, 1, 0, 2, FLOAT, FLOAT, FLOAT],
+                'distort': [800, .7, .7, 1, 0, 1000, 0, 1, 0, 1, 0, 2, FLOAT, FLOAT, FLOAT],
+                'filter': [1000, .6, 0, 1, 200, 5000, 0, 1, 0, 2, 0, 2, FLOAT, FLOAT, INTEGER],
+                'ring': [500, 1, 0, 1, 0, 1000, 0, 1, 0, 5, 0, 2, FLOAT, FLOAT, INTEGER],
+                'reverb': [1.5, 3000, .5, 1, 0, 4, 100, 7000, 0, 1, 0, 2, FLOAT, FLOAT, FLOAT],
+                'harmon': [1.25, .04, .5, 1, 0, 2, 0, 1, 0, 1, 0, 2, FLOAT, FLOAT, FLOAT]}
 
     CONTROL_TYPES = ['lfo', 'rand', 'adsr']
     CONTROL_TYPES_SEL = ['lfosel', 'randsel', 'adsrsel']
