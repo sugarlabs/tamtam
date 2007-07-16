2119fa4741e1ac0b54de6d1cbab145d323c3cafa
Oli <olivier.belanger@umontreal.ca>
2007-07-16 13:51:26 -0400

    remove generator from Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,7 +12,6 @@ from Util.CSoundClient import new_csound_client
 from Util import ControlStream
 from Util import NoteDB
 from Util.NoteDB import Note
-from miniTamTam.RythmGenerator import generator
 from SubActivity import SubActivity
     
 class Welcome(SubActivity):
@@ -65,7 +64,6 @@ class Welcome(SubActivity):
         actVBox.pack_start(buttonBox, False, False, 100)
         self.add(actVBox)
         self.show_all()
-        self.activate_count = 0
 
     def handleLoad(self, widget, data=None):
         chooser = gtk.FileChooserDialog(
@@ -143,32 +141,8 @@ class Welcome(SubActivity):
         self.set_mode(data)
 
     def onActivate(self, arg):
-        def flatten(ll):
-            rval = []
-            for l in ll:
-                rval += l
-            return rval
-        if self.activate_count == 0:
-            i = 0
-            csnd = new_csound_client()
-            beat = 8
-            density = 0.8
-            regularity = 0.77
-            reverb = 0.2
-            for x in flatten( generator('drum3kit', beat, density, regularity, reverb) ):
-                n = Note(0, x.trackId, i, x)
-                i = i + 1
-                csnd.loopPlay(n,1) # add as active
-            csnd.loopSetNumTicks( beat * Config.TICKS_PER_BEAT)
-            csnd.loopSetTick(0)
-            csnd.loopStart()
-        self.activate_count = self.activate_count + 1
         self.show_all()
 
     def onDeactivate(self):
         if self.playStopButton.get_active():
             self.playStopButton.set_active(False)
-        if (self.activate_count == 1):
-            csnd = new_csound_client()
-            csnd.loopPause()
-            csnd.loopClear()
