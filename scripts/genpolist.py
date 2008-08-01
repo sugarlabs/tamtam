2b165333ab06b7d2e57cf7c3803dadfda99e8246
Nat <natcl@hotmail.com>
2008-08-01 17:25:35 -0400

    new bundles

--- scripts/genpolist.py
+++ scripts/genpolist.py
@@ -4,6 +4,6 @@ import os
 
 activities = ['../' + rep for rep in os.listdir('../') if rep.endswith('activity')]
 for activity in activities:
+    print activity
     for f in sorted(os.listdir(os.path.join(activity,'po'))):
         print 'po/' + f
-        
